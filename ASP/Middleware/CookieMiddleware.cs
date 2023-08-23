ac26c8e186f4a6d4c63c627b2b6f6c79028eafdb
Tetez10 <101556702+Tetez10@users.noreply.github.com>
2023-08-23 19:50:41 +0200

    GlobalErrorHandling

---
+++ ASP/Middleware/CookieMiddleware.cs
@@ -0,0 +1,26 @@
+﻿namespace ASP.Middleware
+{
+    public class CookieMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public CookieMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+
+
+            if (context.Request.Cookies.TryGetValue("LastInteractionTime", out string lastInteractionTime))
+            {
+                context.Items["LastInteractionTime"] = lastInteractionTime;
+            }
+
+            await _next(context);
+
+
+        }
+    }
+}
